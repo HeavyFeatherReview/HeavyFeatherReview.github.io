8aa2efbc1690457f4c5223819bb1d67c05f5f83e
Lucy Zhang <spothorse9.lucy@gmail.com>
2020-11-12 22:03:24 -0800

    Adding in newspaper clippings

---
+++ BeingTheMurderedHousekeeper/script.js
@@ -0,0 +1,4 @@
+// Fix the sizes of everything
+let clip1ImageWidth = $("#clip1Image").width();
+console.debug("Set clip1Text width to" + clip1ImageWidth);
+$("#clip1Text").width(clip1ImageWidth - 10);
