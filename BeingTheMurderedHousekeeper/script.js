b7c70f92785e7e6b36a000bbfbb666e9c3679f3d
Lucy Zhang <spothorse9.lucy@gmail.com>
2020-11-13 17:09:51 -0800

    Positioning the elements

--- BeingTheMurderedHousekeeper/script.js
+++ BeingTheMurderedHousekeeper/script.js
@@ -1,7 +1,22 @@
-// Fix the sizes of everything
+// Fix the widths of everything
 var textPadding = [10, 60, 60, 25, 20];
 for (let i = 1; i < 6; i++) {
     let clipImageWidth = $("#clip" + i + "Image").width();
     console.debug("Set clip1Text width to" + clipImageWidth);
-    $("#clip" + i + "Text").width(clipImageWidth - textPadding[i-1]);
+    $("#clip" + i + "Text").width(clipImageWidth - textPadding[i - 1]);
 }
+
+setImagePositions();
+
+function setImagePositions() {
+    let totalWidth = $("#BeingTheMurderedHousekeeper").width();
+    let windowDifference = $(window).width() - totalWidth;
+    // Position the elements
+    let leftPosition = totalWidth - $("#imageContainer2").width() + windowDifference/2;
+    $("#imageContainer2").css("left", leftPosition);
+}
+
+
+$(window).resize(function() {
+    setImagePositions();
+});
