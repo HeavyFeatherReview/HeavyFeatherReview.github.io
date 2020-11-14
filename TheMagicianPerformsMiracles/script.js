48fa62a8e1d6eb53831a5ae27f5dabcfb7be8dcb
Lucy Zhang <spothorse9.lucy@gmail.com>
2020-11-14 11:44:18 -0800

    Generate card designs

---
+++ TheMagicianPerformsMiracles/script.js
@@ -0,0 +1,58 @@
+const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+const suits = ['♥','♦','♠','♣'];
+const suitColors = {
+      '♠': 'black',
+      '♣': 'black',
+      '♦': 'red',
+      '♥': 'red',
+};
+
+// Display cards
+const totalCardNums = 6;
+for (let i = 1; i<=totalCardNums; i++)
+{
+	console.debug("Adding to " + i);
+	let suitIndex = i - 1;
+	if (suitIndex > 3)
+	{
+		suitIndex = suitIndex - (suitIndex - 3);
+	}
+	console.debug("Suit: " + suitIndex);
+	appendCardRankAndSuit("cardFront" + i, ranks[i], suits[suitIndex]);
+}
+
+function appendCardRankAndSuit(divID, rank, suit)
+{
+	let color = suitColors[suit];
+	// Top 
+	$("#" + divID + " .suit-rank-top").html(suit +"<br>"+ rank);
+	// Center
+	$("#" + divID+ " .inner p").append("<div class='card-design suit-middle "+color+"'>"+ suit +"</div>");
+	// Bottom
+	$("#" + divID + " .rank-suit-bottom").html(rank +"<br>"+ suit);
+
+	$("#" + divID + " .card-design").addClass(color);
+
+}
+
+// What do cards look like? Where do the ranks and suits go again? top left and bottom right corner?
+// what about number?
+/*
+shape
+number
+
+
+
+
+
+         more shapes
+
+
+
+
+
+
+
+                   number
+                   shape (inverted)
+*/
