30a9ae1a9cce1fc84b79f8e10453117a1780552e
Lucy Zhang <spothorse9.lucy@gmail.com>
2020-11-13 16:17:06 -0800

    Add the other clippings

--- BeingTheMurderedHousekeeper/script.js
+++ BeingTheMurderedHousekeeper/script.js
@@ -1,4 +1,7 @@
 // Fix the sizes of everything
-let clip1ImageWidth = $("#clip1Image").width();
-console.debug("Set clip1Text width to" + clip1ImageWidth);
-$("#clip1Text").width(clip1ImageWidth - 10);
+var textPadding = [10, 60, 60, 25, 20];
+for (let i = 1; i < 6; i++) {
+    let clipImageWidth = $("#clip" + i + "Image").width();
+    console.debug("Set clip1Text width to" + clipImageWidth);
+    $("#clip" + i + "Text").width(clipImageWidth - textPadding[i-1]);
+}
