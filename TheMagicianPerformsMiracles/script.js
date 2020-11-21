f90dd98a1ae4cb06ab5a2b32c6a3aa375a981009
Lucy Zhang <spothorse9.lucy@gmail.com>
2020-11-20 20:42:01 -0500

    These twinkles work much better

--- TheMagicianPerformsMiracles/script.js
+++ TheMagicianPerformsMiracles/script.js
@@ -96,21 +96,46 @@ function president() {
     console.debug("Make sparkle visible");
     $("#presidentInnerBack").css("background", "black");
     $("#presidentInnerBack").css("color", "white");
-    $("#sparkle").css("visibility", "visible");
-    twinkleInterval = window.setInterval(function() {
-        let randomPercentLeft = getRandomInt(30, 80);
-        let randomPercentTop = getRandomInt(30, 80);
-        console.debug("New coords: left:" + randomPercentLeft + ", top" + randomPercentTop);
-        $("svg").css("left", randomPercentLeft + "%");
-        $("svg").css("top", randomPercentTop + "%");
-    }, 7000);
+    $("#starshine").css("visibility", "visible");
+    startTwinkle();
 }
 
 function cancelPresident() {
-    $("#sparkle").css("visibility", "hidden");
-    $("#presidentInnerBack").css("background", "");
-    $("#presidentInnerBack").css("color", "");
-    window.clearInterval(twinkleInterval);
+    // $("#sparkle").css("visibility", "hidden");
+    // $("#presidentInnerBack").css("background", "");
+    // $("#presidentInnerBack").css("color", "");
+    // window.clearInterval(twinkleInterval);
+    $("#starshine").css("visibility", "hidden");
+}
+
+function startTwinkle() {
+    var body = $('#starshine'),
+        template = $('.template.shine'),
+        stars = 500,
+        sparkle = 20;
+
+
+    var size = 'small';
+    var createStar = function() {
+        template.clone().removeAttr('id').css({
+            top: (Math.random() * 100) + '%',
+            left: (Math.random() * 100) + '%',
+            webkitAnimationDelay: (Math.random() * sparkle) + 's',
+            mozAnimationDelay: (Math.random() * sparkle) + 's'
+        }).addClass(size).appendTo(body);
+    };
+
+    for (var i = 0; i < stars; i++) {
+        if (i % 2 === 0) {
+            size = 'small';
+        } else if (i % 3 === 0) {
+            size = 'medium';
+        } else {
+            size = 'large';
+        }
+
+        createStar();
+    }
 }
 
 var displayCardsInterval = null;
