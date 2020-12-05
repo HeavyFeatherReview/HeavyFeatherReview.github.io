43eaa6facc513c831b218a74871218883c5ff26c
Lucy Zhang <spothorse9.lucy@gmail.com>
2020-12-05 12:26:01 -0500

    The earth appears!!!!

--- CosmicMicros/script.js
+++ CosmicMicros/script.js
@@ -10,15 +10,17 @@ let mesh, uniforms;
 var sphereGeometry;
 
 // Earth params
-var radius = 0.5,
+var radius = 5,
     segments = 32,
     rotation = 6;
+
+var textureLoader = new THREE.TextureLoader();
 
 function createClouds(radius, segments) {
     return new THREE.Mesh(
         new THREE.SphereGeometry(radius + 0.003, segments, segments),
         new THREE.MeshPhongMaterial({
-            map: THREE.ImageUtils.loadTexture('images/fair_clouds_4k.png'),
+            map: textureLoader.load('images/fair_clouds_4k.png'),
             transparent: true
         })
     );
@@ -28,7 +30,7 @@ function createStars(radius, segments) {
     return new THREE.Mesh(
         new THREE.SphereGeometry(radius, segments, segments),
         new THREE.MeshBasicMaterial({
-            map: THREE.ImageUtils.loadTexture('images/galaxy_starfield.png'),
+            map: textureLoader.load('images/galaxy_starfield.png'),
             side: THREE.BackSide
         })
     );
@@ -37,6 +39,8 @@ function createStars(radius, segments) {
 function init() {
 
     scene = new THREE.Scene();
+    scene.background = new THREE.Color(0x000000);
+    console.debug("background set: " + scene.background);
 
     camera = new THREE.PerspectiveCamera(45, WIDTH / HEIGHT, 0.01, 1000);
     //camera.position.z = 1.5;
@@ -47,103 +51,58 @@ function init() {
 
     scene.add(new THREE.AmbientLight(0x333333));
 
-    var clouds = createClouds(radius, segments);
-    clouds.rotation.y = rotation;
-    scene.add(clouds)
+    // var clouds = createClouds(radius, segments);
+    // clouds.rotation.y = rotation;
+    // scene.add(clouds)
 
-    var stars = createStars(90, 64);
-    scene.add(stars);
+    // var stars = createStars(90, 64);
+    // scene.add(stars);
 
 
     //
     var sphereGeometry = new THREE.SphereGeometry(radius, segments, segments);
-    let geometry = sphereGeometry;
-    // geometry.center();
-    const tessellateModifier = new THREE.TessellateModifier(8, 6);
-    geometry = tessellateModifier.modify(geometry);
 
-    //
+    console.debug(sphereGeometry);
 
-    geometry = new THREE.BufferGeometry().fromGeometry(geometry);
 
-    const numFaces = geometry.attributes.position.count / 3;
+    textureLoader.load('images/2_no_clouds_4k.jpg', function(clouds) {
+        textureLoader.load('images/elev_bump_4k.jpg', function(elev_bump) {
+            textureLoader.load('images/water_4k.png', function(water) {
+                mesh = new THREE.Mesh(
+                    sphereGeometry,
+                    new THREE.MeshPhongMaterial({
+                        map: clouds,
+                        bumpMap: elev_bump,
+                        bumpScale: 0.005,
+                        specularMap: water,
+                        specular: new THREE.Color('grey')
+                    })
+                );
 
-    const colors = new Float32Array(numFaces * 3 * 3);
-    const displacement = new Float32Array(numFaces * 3 * 3);
 
-    const color = new THREE.Color();
+                console.debug(mesh);
+                scene.add(mesh);
 
-    for (let f = 0; f < numFaces; f++) {
+                renderer = new THREE.WebGLRenderer({ antialias: true });
+                renderer.setPixelRatio(window.devicePixelRatio);
+                renderer.setSize(WIDTH, HEIGHT);
 
-        const index = 9 * f;
+                const container = document.getElementById('container');
+                container.appendChild(renderer.domElement);
 
-        const h = 0.2 * Math.random();
-        const s = 0.5 + 0.5 * Math.random();
-        const l = 0.5 + 0.5 * Math.random();
+                console.debug("Finished loading everything");
+                console.debug(clouds);
+                console.debug(elev_bump);
+                console.debug(water);
 
-        color.setHSL(h, s, l);
+                controls = new THREE.TrackballControls(camera, renderer.domElement);
 
-        const d = 10 * (0.5 - Math.random());
+                animate();
+            });
+        });
+    });
 
-        for (let i = 0; i < 3; i++) {
-
-            colors[index + (3 * i)] = color.r;
-            colors[index + (3 * i) + 1] = color.g;
-            colors[index + (3 * i) + 2] = color.b;
-
-            displacement[index + (3 * i)] = d;
-            displacement[index + (3 * i) + 1] = d;
-            displacement[index + (3 * i) + 2] = d;
-        }
-    }
-
-    geometry.setAttribute('customColor', new THREE.BufferAttribute(colors, 3));
-    geometry.setAttribute('displacement', new THREE.BufferAttribute(displacement, 3));
-
-    //
-
-    // uniforms = {
-
-    //     amplitude: { value: 0.0 }
-
-    // };
-
-    // const shaderMaterial = new THREE.ShaderMaterial({
-
-    //     uniforms: uniforms,
-    //     vertexShader: document.getElementById('vertexshader').textContent,
-    //     fragmentShader: document.getElementById('fragmentshader').textContent
-
-    // });
-
-    mesh = new THREE.Mesh(
-        sphereGeometry,
-        new THREE.MeshPhongMaterial({
-            map: THREE.ImageUtils.loadTexture('images/2_no_clouds_4k.jpg'),
-            bumpMap: THREE.ImageUtils.loadTexture('images/elev_bump_4k.jpg'),
-            bumpScale: 0.005,
-            specularMap: THREE.ImageUtils.loadTexture('images/water_4k.png'),
-            specular: new THREE.Color('grey')
-        })
-    );
-
-    scene.add(mesh);
-
-    renderer = new THREE.WebGLRenderer({ antialias: true });
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(WIDTH, HEIGHT);
-
-    const container = document.getElementById('container');
-    container.appendChild(renderer.domElement);
-
-    controls = new THREE.TrackballControls(camera, renderer.domElement);
-
-    // stats = new Stats();
-    // container.appendChild(stats.dom);
-
-    //
-
-    window.addEventListener('resize', onWindowResize, false);
+    // window.addEventListener('resize', onWindowResize, false);
 
 }
 
@@ -162,8 +121,6 @@ function animate() {
 
     render();
 
-    stats.update();
-
 }
 
 function render() {
@@ -179,4 +136,4 @@ function render() {
 }
 
 init();
-render();
+//render();
