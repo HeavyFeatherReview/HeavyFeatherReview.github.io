01f8c5d72a2e354d93363b96893b67a57ce96a27
Lucy Zhang <spothorse9.lucy@gmail.com>
2020-12-20 15:16:56 -0500

    Format text

--- BitterNails/script.js
+++ BitterNails/script.js
@@ -1,20 +1,18 @@
 $('.mail-choice').change(function() {
-    if($(this).is(":checked")) {
+    if ($(this).is(":checked")) {
         $(this).parent().addClass('selected-bg');
-  } else {
-    $(this).parent().removeClass('selected-bg');
-  }
+    } else {
+        $(this).parent().removeClass('selected-bg');
+    }
+    // Show the text
+    let index = $("input").index($(this));
+    console.debug("Selected: " + index);
+    $(".mail-contents").each(function(i, value) {
+        if (i == index) {
+            // Show the element
+            $(this).css("display", "block");
+        } else {
+            $(this).css("display", "none");
+        }
+    });
 });
-
-const colorInput = document.getElementById("colorpicker");
-
-colorInput.addEventListener("input", (e) => {
- document.body.style.setProperty("--button-color", e.target.value);
-});
-
-$('.inbox-calendar').click(function(){
-  $('.calendar-container').toggleClass('calendar-show');
- $('.inbox-container').toggleClass('hide');
- $('.mail-detail').toggleClass('hide'); 
-});
-
