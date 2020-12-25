d43cbbfb2cad02e0ee3a176e93be269f961e8106
Lucy Zhang <spothorse9.lucy@gmail.com>
2020-12-24 21:18:26 -0500

    Be able to add tasks

--- BitterNails/script.js
+++ BitterNails/script.js
@@ -1,5 +1,5 @@
 $('.mail-choice').change(function() {
-	var selectedElement = $(this);
+    var selectedElement = $(this);
     if ($(this).is(":checked")) {
         $(this).parent().addClass('selected-bg');
     }
@@ -22,5 +22,22 @@ var n = d.toString();
 $("#todays-date").html(n);
 
 $("#create").click(function() {
-  $(this).before("<textarea class='cursive'>Define Success for Yourself</textarea>");
+    $(this).before("<textarea class='cursive'>Define Success for Yourself</textarea>");
+});
+
+var addedTask = 0;
+
+$(".add-task .add-button").click(function() {
+    addedTask += 1;
+    $(".inbox").append('<div class="msg selected-bg anim-y">' +
+        '<input type="checkbox" name="msg" id="mailAdded' + addedTask + '" class="mail-choice" checked>' +
+        '<label for="mailAdded' + addedTask + '"></label>' +
+        '<div class="msg-content">' +
+        '<div class="msg-title">Test task</div>' +
+        '<div class="msg-date">' + n + '</div>' +
+        '</div>' +
+        '</div>');
+
+    let scr = $('.inbox')[0].scrollHeight;
+    $('.inbox').animate({scrollTop: scr}, 500);
 });
